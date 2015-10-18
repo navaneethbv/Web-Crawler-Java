cec2092c091891e0450fd91b1bbce2ff9dd99fb4
Navaneeth <navaneethbv@gmail.com>
2015-10-17 20:17:57 -0500

    Simple Web Crawler
    Signed-off-by: Navaneeth <navaneethbv@gmail.com>

---
+++ src/com/web/crawler/SpiderTest.java
@@ -0,0 +1,15 @@
+package com.web.crawler;
+public class SpiderTest
+{
+    /**
+     * This is our test. It creates a spider (which creates spider legs) and crawls the web.
+     * 
+     * @param args
+     *            - not used
+     */
+    public static void main(String[] args)
+    {
+        Spider spider = new Spider();
+        spider.search("http://www.utdallas.edu/", "bus");
+    }
+}
