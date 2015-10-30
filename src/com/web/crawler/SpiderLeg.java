6f4d2d6c9378a28e7c27a528a22843c5eba3eb15
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-10-29 19:07:00 -0500

    Update SpiderLeg.java

--- src/com/web/crawler/SpiderLeg.java
+++ src/com/web/crawler/SpiderLeg.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
  
 import org.jsoup.Connection; 
-import org.jsoup.Jsoup;
+import org.jsoup.Jsoup; 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
